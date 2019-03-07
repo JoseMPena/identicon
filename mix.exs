7074b57135d92039f627f5248abda91b2cd0c274
Jose M Peña <josmpena91@gmail.com>
2019-03-07 09:52:05 +0100

    remote and docs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,17 @@ defmodule Identicon.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "Identicon",
+      source_url: "https://github.com/JoseMPena/identicon",
+      homepage_url: "",
+      docs: [
+        main: "Identicon", # The main page in the docs
+        #logo: "",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -23,6 +33,7 @@ defmodule Identicon.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
   end
 end
