a86c1973ea70c75fb78698d4e8e90ae8333de16a
Jose M Peña <josmpena91@gmail.com>
2019-04-08 10:24:10 +0200

    Filtered grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,18 +3,41 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+    |> filter_odd_squares
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn ({code, _}) ->
+      rem(code, 2) == 0
+    end
 
-    [r, g , b]
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
-    |> :binary.bin_to_list
+          |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1) # pass function reference as argument
+      |> List.flatten
+      |> Enum.with_index    # adds indexes to list
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    row ++ [second, first]
   end
 end
